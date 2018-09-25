3374dd2c9db24b822fc7525e25b298c30496ab07
Karol Siwek <siwek.karol1992@gmail.com>
2018-09-25 19:48:54 +0200

    Zadanie 1

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,22 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int x = 0;
-        int suma = 0;
+        double a = 0;
+        double b = 0;
+        double c = 0;
+        double x = 0;
+
         Scanner scan = new Scanner(System.in);
-        System.out.println("Podaj liczbę");
-        x = scan.nextInt();
-        while (x * x < 100) {
-            suma = suma + x;
-            x = scan.nextInt();
-        }
-        suma = suma % 2;
-        System.out.println(suma);
+        do {
+            System.out.println("Podaj liczbę a");
+            a = scan.nextDouble();
+        } while (a == 0);
+        System.out.println("Podaj liczbę b");
+        b = scan.nextDouble();
+        System.out.println("Podaj liczbę c");
+        c = scan.nextDouble();
+
+        x = (c - b) / a;
+        System.out.println(x);
     }
 }
